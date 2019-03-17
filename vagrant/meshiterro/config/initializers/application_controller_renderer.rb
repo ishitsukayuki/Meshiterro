29b7226df84844b7cd016cd831a2aa14a9768cc2
ishitsukayuki <19961017yuki@gmail.com>
2019-03-17 22:49:48 +0900

    [Finish]5-3

--- vagrant/meshiterro/config/initializers/application_controller_renderer.rb
+++ vagrant/meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'db7ba75b2c117c0fa93c7d4e6238ee2efba1cd5b831aa7d7f9131ab3d3a0c64dc043f41f462a3cc228d548b9b2bcacfaecad5a692604110de2056252b89fbb25'
